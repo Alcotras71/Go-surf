d8b9a92800d3de78419e4b73c46fbd345a02c4de
Andrey <aimdragman2@yandex.ru>
2020-11-13 14:25:31 +0300

    main-page

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,18 @@
 $(function(){
 
+	$('.header__slider').slick({
+		infinite: true,
+		fade: true,	
+		prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/main/arrow-left.svg" alt="arrow-left" />',
+		nextArrow: '<img class="slider-arrows slider-arrows__rigth" src="images/main/arrow-right.svg" alt="arrow-rigth" />',
+		asNavFor: '.slider-dots',
+		dots: true
+	});
+
+	$('.slider-dots').slick({
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		asNavFor: '.header-slider',
+	});
+
 });
