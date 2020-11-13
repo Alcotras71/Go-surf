721175b2c0c0eabf6054c7ce8ac2ef417413d035
Andrey <aimdragman2@yandex.ru>
2020-11-14 00:58:37 +0300

    travel-page

--- app/js/main.js
+++ app/js/main.js
@@ -15,4 +15,27 @@ $(function(){
 		asNavFor: '.header-slider',
 	});
 
+	$('.surf__slider').slick({
+		slidesToShow:4,
+		slidesToScroll: 1,
+		prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/main/arrow-left.svg" alt="arrow-left" />',
+		nextArrow: '<img class="slider-arrows slider-arrows__rigth" src="images/main/arrow-right.svg" alt="arrow-rigth" />',
+		asNavFor: '.slider-map'
+	});
+
+	$('.slider-map').slick({
+		slidesToShow: 8,
+		slidesToScroll: 1,
+		arrows: false,
+		asNavFor: '.surf__slider',
+		focusOnSelect: true
+	});
+
+	$('.travel__slider').slick({
+		infinite: true,
+		fade: true,	
+		prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/main/arrow-left.svg" alt="arrow-left" />',
+		nextArrow: '<img class="slider-arrows slider-arrows__rigth" src="images/main/arrow-right.svg" alt="arrow-rigth" />'
+	});
+
 });
